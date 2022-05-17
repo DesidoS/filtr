b7d942b7d8bf36ff0e506bb4f2724a24888b3bdc
Oleksii Kravchenko <djdesidos@gmail.com>
2022-05-17 14:30:27 +0300

    add

---
+++ filtr.js
@@ -0,0 +1,9 @@
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-filtr-button]");
+//   const mobileMenuRef = document.querySelector("[filtr-menu]");
+
+//     menuBtnRef.addEventListener("click", () => {
+      
+//     mobileMenuRef.classList.toggle("portfBlock");
+//   });
+// })();
